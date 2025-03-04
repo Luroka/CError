dad873ec84352b10667eb48f36a0706e943f7efa
chris <christophleptin@gmail.com>
2025-03-04 11:00:41 +0100

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,56 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace sf;
+
+int klicker();
+
+bool active = true;
+
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    std::vector<std::thread> threads;
+
+    auto window = sf::RenderWindow(sf::VideoMode({500, 500}), "Menu");
+    window.setFramerateLimit(144);
+
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                active = false;
+                window.close();
+            }
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::Key::Space))
+        {   
+            threads.push_back(std::thread(klicker));
+            sf::sleep(sf::milliseconds(150));
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    //Es wird gewartet bis alle threads beendet sind
+    for (auto& th : threads) {
+        if (th.joinable())
+        {
+            th.join();
+        }
+    }
+}
+
+int klicker() 
+{
+    auto window = sf::RenderWindow(sf::VideoMode({500, 500}), "Klicker");
     window.setFramerateLimit(144);
 
     while (window.isOpen())
@@ -15,7 +63,14 @@ int main()
             }
         }
 
+        if (active == false) 
+        {
+            window.close();
+        }
+
         window.clear();
         window.display();
     }
+
+    return 0;
 }
